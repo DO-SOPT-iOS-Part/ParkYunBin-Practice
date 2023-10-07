08a5ab597aacba01777007b06099571a632cf265
binisnil <dbsqlssss0704@gmail.com>
2023-10-07 14:19:37 +0900

    [Chore] 초기 세팅

---
+++ ParkYunBin-practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParkYunBin-practice
+//
+//  Created by 박윤빈 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

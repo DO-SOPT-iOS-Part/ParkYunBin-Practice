5f023f1ce02fe8a912716b127308f3531021412a
binisnil <dbsqlssss0704@gmail.com>
2023-10-13 16:48:17 +0900

    [Feat] 아이디, 비밀번호 입력 다 안했을 시, 화면 전환 막기

--- ParkYunBin-practice/ViewController.swift
+++ ParkYunBin-practice/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     private var idText: String = ""
     private var passwordText: String = ""
+    @IBOutlet var loginButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
     @IBAction func didButtonTapped(_ sender: Any) {
         print("\(idText)\n\(passwordText)")
@@ -34,12 +36,23 @@ class ViewController: UIViewController {
         }
     }
     
+    func checkInputState(idInput: String, passwordInput: String) -> Bool {
+        if idInput != "" && passwordInput != "" {
+            return true
+        } else {
+            view.showToast(message: "아이디 및 비밀번호를 모두 입력해주세요!")
+            return false
+        }
+    }
+    
     func pushToResultVC() {
-        
-        guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
-        resultVC.bindText(email: idText, password: passwordText)
+        if checkInputState(idInput: idText, passwordInput: passwordText) {
+            
+            guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
+            resultVC.bindText(email: idText, password: passwordText)
             self.navigationController?.pushViewController(resultVC, animated: true)
-        
+            
+        }
     }
         
     func presentToResultVC() {

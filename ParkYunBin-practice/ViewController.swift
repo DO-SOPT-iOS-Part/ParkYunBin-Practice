9bbe312d2341e882598abb3db7619224eae0a6cf
binisnil <dbsqlssss0704@gmail.com>
2023-10-14 14:35:15 +0900

    [Chore] #2 2주차 practice 파일 세팅

--- ParkYunBin-practice/ViewController.swift
+++ ParkYunBin-practice/ViewController.swift
@@ -2,48 +2,18 @@
 //  ViewController.swift
 //  ParkYunBin-practice
 //
-//  Created by 박윤빈 on 2023/10/07.
+//  Created by 박윤빈 on 2023/10/14.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    private var idText: String = ""
-    private var passwordText: String = ""
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
-    @IBAction func didButtonTapped(_ sender: Any) {
-        print("\(idText)\n\(passwordText)")
-        pushToResultVC()
-    }
-    
-    @IBAction func idTextFieldDidTapped(_ sender: Any) {
-        guard let textField = sender as? UITextField else {return}
-        if let idText = textField.text {
-            self.idText = idText
-        }
-    }
-    
-    @IBAction func pwTextFieldDidTapped(_ sender: Any) {
-        guard let textField = sender as? UITextField else {return}
-        if let passwordText = textField.text {
-            self.passwordText = passwordText
-        }
-    }
-    
-    func pushToResultVC() {
-        guard let resultVC = self.storyboard?.instantiateViewController(identifier: "ResultViewController") as? ResultViewController else {return}
-        resultVC.bindText(email: idText, password: passwordText)
-        self.navigationController?.pushViewController(resultVC, animated: true)
-    }
-        
-    func presentToResultVC() {
-        guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
-        resultVC.bindText(email: idText, password: passwordText)
-        self.present(resultVC, animated: true)
-    }
-}
 
+
+}

88a6c3a2b0ec79a99395cd7fb5ae7c33cc438e23
binisnil <dbsqlssss0704@gmail.com>
2023-10-14 22:06:31 +0900

    [Feat] #2 scrollView 실습

--- ParkYunBin-practice/SceneDelegate.swift
+++ ParkYunBin-practice/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             guard let windowScene = (scene as? UIWindowScene) else { return }
             
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = PracticeStackViewVC()
+            window.rootViewController = PracticeScrollViewVC()
             window.makeKeyAndVisible()
             self.window = window
         }

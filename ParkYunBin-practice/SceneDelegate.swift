1fda8723f56d011bced15a9aabf8e81e96cec7b2
binisnil <dbsqlssss0704@gmail.com>
2023-10-28 14:44:07 +0900

    [Fix] 깃허브 문제 수정 중

---
+++ ParkYunBin-practice/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  ParkYunBin-practice
+//
+//  Created by 박윤빈 on 2023/10/07.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        window?.overrideUserInterfaceStyle = .light
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        navigationController.isNavigationBarHidden = true
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+    }
+}
+
---
+++ ParkYunBin-practice/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ParkYunBin-practice
+//
+//  Created by 박윤빈 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
+
